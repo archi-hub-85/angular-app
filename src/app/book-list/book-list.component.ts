61d3ccda3ddabe2e971c18d05ccbb1383ff06d27
Artur Khubbutdinov <archi_hub@mail.ru>
2021-07-16 12:41:33 +0500

    Added unsubscribe() to BookListComponent.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Subscription } from 'rxjs';
 
 import { Book } from '../dto';
 import { BookService } from '../book-service/book.service';
@@ -11,10 +12,11 @@ import { BookService } from '../book-service/book.service';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit, AfterViewInit {
+export class BookListComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  displayedColumns: string[] = ['id', 'title', 'year', 'author'];
+  readonly displayedColumns: string[] = ['id', 'title', 'year', 'author'];
   dataSource = new MatTableDataSource<Book>();
+  private subscription: Subscription | undefined;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -33,11 +35,15 @@ export class BookListComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.bookService.getBooks().subscribe(books => {
+    this.subscription = this.bookService.getBooks().subscribe(books => {
         this.dataSource.data = books;
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
