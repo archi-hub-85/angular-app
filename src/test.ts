8d1774bf384032f8d30c9795943b3987ae433c2a
Artur Khubbutdinov <archi_hub@mail.ru>
2021-07-29 17:56:39 +0500

    Fixed 'style' leaks between calls of different tests.

--- src/test.ts
+++ src/test.ts
@@ -17,7 +17,8 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+  platformBrowserDynamicTesting(),
+  { teardown: { destroyAfterEach: true } } // https://github.com/angular/angular/pull/42566
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
